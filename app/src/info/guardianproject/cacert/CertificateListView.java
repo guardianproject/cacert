f258bc65dac81ac54ebe1a67427d5c97d280d290
Nathan Freitas <nathan@freitas.net>
2011-09-04 22:20:30 -0400

    updated licensing info in source

--- app/src/info/guardianproject/cacert/CertificateListView.java
+++ app/src/info/guardianproject/cacert/CertificateListView.java
@@ -1,3 +1,6 @@
+/* Copyright (c) 2011, Nathan Freitas,/ The Guardian Project - https://guardianproject.info */
+/* See LICENSE for licensing information */
+
 package info.guardianproject.cacert;
 
 public class CertificateListView
