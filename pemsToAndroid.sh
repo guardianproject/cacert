7b56fbdf649f926269b1c83486518bfe7b2c1df0
n8fr8 <nathan@freitas.net>
2012-11-05 16:57:05 +0530

    updated generator script to take arguments

--- pemsToAndroid.sh
+++ pemsToAndroid.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #This script creates an Android compatibility certificate store from the current installed Debian certs in /etc/ssl/certs
 
-certStore="cacerts.bks"
-pemPath="/your/path/to/pems/*.pem"
+certStore=$1
+pemPath="$2"
 
 rm $certStore
 rm $certStore.txt
@@ -11,7 +11,7 @@ certAliasNum=0
 
 for file in `dir -d $pemPath` ; do
 	certAliasNum=$((certAliasNum+1))
-	keytool -importcert -v -trustcacerts -file "$file" -alias $certAliasNum -keystore "$certStore" -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath "libs/bcprov-jdk15on-147.jar" -storetype BKS -storepass changeit -noprompt
-	keytool -list -v -keystore "$certStore" -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath "libs/bcprov-jdk15on-147.jar" -storepass changeit -alias $certAliasNum >> $certStore.txt
+	keytool -importcert -v -trustcacerts -file "$file" -alias $certAliasNum -keystore "$certStore" -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath "libs/bcprov-jdk16-145.jar" -storetype BKS -storepass changeit -noprompt
+	keytool -list -v -keystore "$certStore" -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath "libs/bcprov-jdk16-145.jar" -storepass changeit -alias $certAliasNum >> $certStore.txt
 done
 
